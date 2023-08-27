0e7cf0df818dba8c02f27fbbaf85bd28ab231826
zahidari <mzhdar1@gmail.com>
2023-08-27 23:00:01 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author github.com/zahidari
+ * 
+ *       By listing the first six prime numbers: 2,3,5,7,11 and 13, 
+ *       we can see that the 6 th prime is 13.
+ *
+ *       What is the 10 001 st prime number?
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+       int prime = 2;
+       int nThPrime = 6;
+       int countTheNth = 0; 
+       
+       for(int i = 2; i < 20; i++){
+           
+       }
+
+       
+    }
+    
+}
