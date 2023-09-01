7c95b5e5b8c6a1d9587e8965ca15818b7a55f164
zahidari <mzhdar1@gmail.com>
2023-09-01 23:33:56 +0200

    almost finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,35 +6,40 @@
  *       we can see that the 6 th prime is 13.
  *
  *       What is the 10 001 st prime number?
+ *       
+ *       We are decresing every time the n'th prime integer by 1 if we find a prime number.
+ *       at the end we are gi,ongto find the n'th Prime number.
  */
 public class Main {
     
-
+ /** @brief:   is the number x prime ?
+    *   return: if the number is prime than returns true, else false
+    */
+    boolean isItPrime(int x) 
+    { 
+        for (int i = 2; i <= x / 2; ++i){
+            if (x % i == 0){return false;}
+        }  
+        return true;
+    }
 
     public static void main(String[] args) {
         
-       int prime = 2;
-       int nThPrime = 6;
-       int countTheNth = 0; 
+        //number to iterate and find the primes.
+        int num = 2;
+        int nThPrime = 6;
        
-       for(int i = 0;  i < nThPrime;)
-
- 
-    }//end of main
-    
-    
-    
-       /** @brief:   is the number x prime ?
-        *   return: if prime than true, else false
-        */
-        boolean isItPrime(int x) { 
-
-            for (int i = 2; i <= x / 2; ++i)
-                if (x % i == 0)
-                return false;
-            return true;
-
-        }
         
+       
+        while(nThPrime != 0){
+           if(isItPrime(num))
+               nThPrime -=1;
+           num++;
+           
+           
+        }
+        System.out.println("the n'th prime is: "+num);
+ 
+    }// end of main   
 }
 
