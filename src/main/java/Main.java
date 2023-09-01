b22f88b9d7b3373ca6116e20d81865d87a97a3bc
zahidari <mzhdar1@gmail.com>
2023-09-02 00:08:36 +0200

    Changes made:
    1. Added a clear problem description at the beginning with a reference to Project Euler Problem 7.
    2. Improved comments to explain the code's purpose and how it works.
    3. Enhanced the `isItPrime` function to handle edge cases (0 and 1) and optimized the loop range to check for primality.
    4. Updated the variable names to be more descriptive.
    5. Improved code formatting and indentation for readability.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,60 @@
 /**
- *
- * @author github.com/zahidari
+ * Project Euler Problem 7: 10001st Prime
  * 
- *       By listing the first six prime numbers: 2,3,5,7,11 and 13, 
- *       we can see that the 6 th prime is 13.
+ *  @author github.com/zahidari
+ * 
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+ * we can see that the 6th prime is 13.
  *
- *       What is the 10 001 st prime number?
- *       
- *       We are decresing every time the n'th prime integer by 1 if we find a prime number.
- *       At the end the iteration is finished and we are going to find the n'th Prime number.
+ * What is the 10,001st prime number?
+ * 
+ * We iterate through integers, checking if each number is prime.
+ * When we find a prime, we decrement the target count (nThPrime) until it reaches zero.
+ * At the end of the iteration, we will have found the 10,001st prime number.
  */
 public class Main {
-    
- /** @brief:   is the number x prime ?
-    *   return: if the number is prime than returns true, else false
-    */
-    static boolean isItPrime(int x) 
-    { 
-        for (int i = 2; i <= x / 2; ++i){
-            if (x % i == 0){return false;}
-        }  
+
+    /**
+     * Checks if a number x is prime.
+     *
+     * @param x The number to check for primality.
+     * @return true if x is prime, false otherwise.
+     */
+    static boolean isItPrime(int x) {
+        if (x <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(x); ++i) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 
     public static void main(String[] args) {
-        
-        //number to iterate and find each primes b.
-        int num = 2;
-        
-        //the number you are looking for
-        int nThPrime = 10001;
-       
-        
-       
-        while(true){
-            
-            //is the num a prime number,
-            //than each time  the nThPrime is decreasing
-           if(isItPrime(num))
-               nThPrime -=1;
-           
-           //control of the 
-           if(nThPrime == 0)
-               break;
-           
-           num++;
-           
-           
-        }
-        System.out.println("the n'th prime is: "+num);
- 
-    }// end of main   
-}
 
+        // Number to iterate and find each prime.
+        int num = 2;
+
+        // The nth prime you are looking for.
+        int nThPrime = 10_001;
+
+        while (true) {
+
+            // If the num is a prime number, decrement nThPrime.
+            if (isItPrime(num)) 
+                nThPrime -= 1;
+            
+
+            // If we have found the 10,001st prime, exit the loop.
+            if (nThPrime == 0) 
+                break;
+            
+
+            num++;
+        }
+        
+        System.out.println("The 10,001st prime is: " + num);
+    }// end of main
+}
